b264110fd1b98d48e7a8034206fe425a31f764db
deing <admin@15318.de>
2019-10-31 17:17:47 +0100

    Some cleanup

--- src/views/cap_poll/main.cr
+++ src/views/cap_poll/main.cr
@@ -1,6 +1,6 @@
-def gen_poll(res)
+def gen_poll(cap)
   og_desc = ""
-  case res.kind
+  case cap.kind
   when CapKind::PollAdmin
     og_desc = "You were supposed to keep this link private, s m h"
   when CapKind::PollVote
@@ -8,39 +8,13 @@ def gen_poll(res)
   else
     og_desc = "View this poll on Tug of Vote"
   end
-  poll = DATABASE.query_all("select * from polls where id = ?", res.poll_id, as: Poll)[0]
-  view_component = gen_view(res)
-  vote_component = gen_vote(res)
-  admin_component = gen_admin(res)
-  render "src/ecr/cap_main.ecr"
-end
-
-def gen_view(cap)
   poll = DATABASE.query_all("select * from polls where id = ?", cap.poll_id, as: Poll)[0]
   votes = DATABASE.query_all("select * from votes where poll_id = ?", cap.poll_id, as: Vote)
   lower_caps = DATABASE.query_all("select * from caps where poll_id = ? and kind <= ? and kind > ?", cap.poll_id, cap.kind_val, CapKind::Revoked.value, as: Cap)
 
-  pro_votes = votes.select { |x| x.kind == VoteKind::InFavor }
-  con_votes = votes.select { |x| x.kind == VoteKind::Against }
-  neu_votes = votes.select { |x| x.kind == VoteKind::Neutral }
+  pro_votes = {votes.select { |x| x.kind == VoteKind::InFavor && !x.reason.empty? }, votes.select { |x| x.kind == VoteKind::InFavor && x.reason.empty? }}
+  con_votes = {votes.select { |x| x.kind == VoteKind::Neutral && !x.reason.empty? }, votes.select { |x| x.kind == VoteKind::Neutral && x.reason.empty? }}
+  neu_votes = {votes.select { |x| x.kind == VoteKind::Against && !x.reason.empty? }, votes.select { |x| x.kind == VoteKind::Against && x.reason.empty? }}
   anonymize = cap.kind == CapKind::PollViewAnon
-
-  render "src/ecr/cap_component_view.ecr"
-end
-
-def gen_vote(cap)
-  # return early if unauthorized.
-  if cap.kind != CapKind::PollVote && cap.kind != CapKind::PollAdmin
-    return ""
-  end
-  render "src/ecr/cap_component_vote.ecr"
-end
-
-def gen_admin(cap)
-  # return early if unauthorized.
-  if cap.kind != CapKind::PollAdmin
-    return ""
-  end
-  poll = DATABASE.query_all("select * from polls where id = ?", cap.poll_id, as: Poll)[0]
-  render "src/ecr/cap_component_admin.ecr"
+  render "src/ecr/cap_poll.ecr"
 end
