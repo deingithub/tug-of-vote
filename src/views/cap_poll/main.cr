9918f2a2e2e16bb0ad4038fad1090213975cc225
Cassidy Dingenskirchen <admin@15318.de>
2019-11-02 21:05:18 +0100

    Fix neutral and against votes being swapped

--- src/views/cap_poll/main.cr
+++ src/views/cap_poll/main.cr
@@ -13,8 +13,8 @@ def gen_poll(cap)
   lower_caps = DATABASE.query_all("select * from caps where poll_id = ? and kind <= ? and kind > ?", cap.poll_id, cap.kind_val, CapKind::Revoked.value, as: Cap)
 
   pro_votes = {votes.select { |x| x.kind == VoteKind::InFavor && !x.reason.empty? }, votes.select { |x| x.kind == VoteKind::InFavor && x.reason.empty? }}
-  con_votes = {votes.select { |x| x.kind == VoteKind::Neutral && !x.reason.empty? }, votes.select { |x| x.kind == VoteKind::Neutral && x.reason.empty? }}
-  neu_votes = {votes.select { |x| x.kind == VoteKind::Against && !x.reason.empty? }, votes.select { |x| x.kind == VoteKind::Against && x.reason.empty? }}
+  neu_votes = {votes.select { |x| x.kind == VoteKind::Neutral && !x.reason.empty? }, votes.select { |x| x.kind == VoteKind::Neutral && x.reason.empty? }}
+  con_votes = {votes.select { |x| x.kind == VoteKind::Against && !x.reason.empty? }, votes.select { |x| x.kind == VoteKind::Against && x.reason.empty? }}
   anonymize = cap.kind == CapKind::PollViewAnon
   render "src/ecr/cap_poll.ecr"
 end
