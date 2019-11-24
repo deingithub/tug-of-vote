3c5da40bf0841f17730e506eb0da70cc302ddc4e
Cassidy Dingenskirchen <admin@15318.de>
2019-11-24 18:07:44 +0100

    Fix confirmation message

--- public/tov.js
+++ public/tov.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     dangerousThings = document.querySelectorAll("[data-js-confirm]");
     for (thing of dangerousThings) {
         thing.addEventListener("click", function (event) {
-            if (!window.confirm(thing.getAttribute("data-js-confirm"))) {
+            if (!window.confirm(event.target.getAttribute("data-js-confirm"))) {
                 event.preventDefault();
             }
         });
